17866207d79f9e9b5adc60199cd2d68dea4da89c
Robson Formigão <rformigao.gomes@gmail.com>
2020-10-21 18:18:33 -0300

    feat(BrowserName): change jquery to js vanila

--- index.js
+++ index.js
@@ -1,52 +1,57 @@
 (function () {
-  
-  'use strict';
+  "use strict";
 
   function browserInfo() {
-    
     var userAgent = navigator.userAgent;
-    
+
     return {
-      
-      browser:   userAgent.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [],
+      browser:
+        userAgent.match(
+          /(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i
+        ) || [],
 
       getBrowser: function getBrowser() {
         var browser = this.browser;
-        
-        if ( this.browser[1] === 'Trident' ) browser = this.isIe();
-        
-        if ( this.browser[1] === 'Chrome' ) {
-          if ( this.isEdge() )  browser = this.isEdge();
-          if ( this.isOpera() ) browser = this.isOpera();
+
+        if (this.browser[1] === "Trident") browser = this.isIe();
+
+        if (this.browser[1] === "Chrome") {
+          if (this.isEdge()) browser = this.isEdge();
+          if (this.isOpera()) browser = this.isOpera();
         }
-        
+
         return {
-          complet : browser[0],
-          name    : browser[1].toLowerCase(),
-          version : browser[2]
-        }        
+          complet: browser[0],
+          name: browser[1].toLowerCase(),
+          version: browser[2],
+        };
       },
 
       isIe: function isIe() {
         var ie = this.browser;
-        return [ ie[0], 'ie', ie[2] ];
+        return [ie[0], "ie", ie[2]];
       },
 
       isEdge: function isEdge() {
         var edge = userAgent.match(/(edge(?=\/))\/?\s*(\d+)/i) || [];
-        if ( edge[1] === 'Edge' ) return edge;
+        if (edge[1] === "Edge") return edge;
       },
 
       isOpera: function isOpera() {
         var opera = userAgent.match(/(OPR(?=\/))\/?\s*(\d+)/i) || [];
-        if ( opera[1] === 'OPR' ) return opera;
+        if (opera[1] === "OPR") return opera;
       },
 
-      addBrowserToBody: function addBrowserToBody () {
+      addBrowserToBody: function addBrowserToBody() {
         var browser = browserInfo().getBrowser().name;
-        $('body').addClass( browser );
-      }
-    }
+        var body = document.getElementsByTagName("body");
+
+        if (body.length) {
+          body[0].classList.add(browser);
+        }
+      },
+    };
   }
-  console.log(browserInfo().getBrowser());
+
+  return browserInfo();
 })();
